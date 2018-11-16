bd82e1006b13f8c2c535926aef878e6d9f62b8b5
Glen Langston <glen.i.langston@gmail.com>
2018-11-16 11:58:28 -0500

    Revised event capture

--- python/ra_event.py
+++ python/ra_event.py
@@ -1,5 +1,6 @@
 """
-Vector Sum channel ranges for radio astronomy
+Event Detection for a sample series for radio astronomy
+Glen Langston (National Science Foundation)
 """
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
@@ -42,61 +43,68 @@ class ra_event(gr.sync_block):
     Peak magnitude^2
     SigmaSquared^
     """
-    def __init__(self, vlen, nsigma, sampleRate, configFileName):
+    def __init__(self, vlen, nsigma, sample_rate, configFileName):
         """
         Initialize the event class, zero sample buffer
         """
         gr.sync_block.__init__(self, name="ra_event",
-                               in_sig=[np.complex32],   # in 1 spectrum
+                               in_sig=[np.complex],   # in 1 spectrum
                                out_sig=[np.float32, np.float32])   # out 2 scalar values
         self.vlen = int(vlen)
         self.vlen2 = int(vlen/2)
-        self.nsigma = float( nsigma)
+        self.nsigma = float(nsigma)
         self.nsigma2 = self.nsigma*self.nsigma
-        self.sampleRate = float( sampleRate)
-        if self.sampleRate < 10.:
-            print 'Invalid Sample Rate: ',self.sampleRate, ' Hz' 
+        self.sample_rate = float(sample_rate)
+        if self.sample_rate < 10.:
+            print 'Invalid Sample Rate: ', self.sample_rate, ' Hz'
             exit()
         # compute time offset between current time and event
-        self.dt = float(self.vlen2)/self.sampleRate
-        print 'ra_event Vlen, Nsigma, dt: ',self.vlen, self.nsigma, self.dt
+        self.dt = float(self.vlen2)/self.sample_rate
+        print 'ra_event Vlen, Nsigma, dt: ', self.vlen, self.nsigma, self.dt
         if self.vlen < 16:
             print 'Not Enough samples (<16) to measure RMS: ', self.vlen
             exit()
-        self.initBuffer( self.vlen)
-        self.configFileName = str( configFileName)
-            
-    def initBuffer( self, vlen):
+        self.init_buffer()
+        self.configFileName = str(configFileName)
+
+    def init_buffer(self):
         """
         Initialize the circular buffer at start and after each event is detected
         """
         # define variables to keep track of the circular buffer
+        self.vlen2 = int(self.vlen/2)
         self.n = int(0) # n is the number of samples in the circular buffer
-        self.next = int(0) # next is where the next sample will be placed 
+        self.next = int(0) # next is where the next sample will be placed
         self.next2 = int(self.vlen2)  # next2 sample examined for an event
         # keep magnitudes
-        self.mags = np.zeros( vlen, dtype=np.complex)
-        self.rmss = np.zeros( vlen, dtype=np.complex)
+        self.mags = np.zeros(self.vlen, dtype=np.complex)
+        self.rmss = np.zeros(self.vlen, dtype=np.complex)
         self.ave = 0.
         self.rms = 0.
+        self.rmssum = 0.
         self.sum = 0.
         self.sum2 = 0.
-        self.oneovern = 1./float(vlen)
+        self.oneovern = 1./float(self.vlen)
+        self.oneovern2 = self.oneovern*self.oneovern
         self.full = False              # start with circular buffer empty
         self.event = False             # start assuming no events
-        self.utcend = datetime.datetime.utcnow() 
-        self.utcevent = self.utcend - self.dt
-        self.utcstart = self.utcvent - self.dt
+        self.utcend = datetime.datetime.utcnow()
+        self.utcevent = self.utcend - datetime.timedelta(seconds=self.dt)
+        self.utcstart = self.utcevent - datetime.timedelta(seconds=self.dt)
+        self.magnitude = 0.
+        self.stddev = 0.
 
-    def writeevent():
+    def writeevent(self):
         """
         writeevent() writes an ascii file containing the observing setup
         and the data stream
         """
-        self.utcend = datetime.datetime.utcnow() 
+        self.utcend = datetime.datetime.utcnow()
         self.utcevent = self.utcend - self.dt
         self.utcstart = self.utcvent - self.dt
-        
+        print 'Utc event: ', self.utcevent, self.magnitude, ' +/- ', self.st
+        print 'Magnitude: ', self.magnitude, ' +/- ', self.stddev
+
     def forecast(self, noutput_items, ninput_items):
         """
         Indicate the number of inputs required to get 1 output spectrum
@@ -120,16 +128,16 @@ class ra_event(gr.sync_block):
         outb = output_items[1]  # all outputs in PORT 1
 
         # get the length of one input
-        for j in range (ns):
+        for j in range(ns):
             # if buffer is already full, must replace value with new
-            if (self.full):
+            if self.full:
 #                self.sum = self.sum - self.values[self.next]
                 self.rmssum = self.rmssum - self.value2s[self.next]
-                    
+
             # record the new values (maybe on top of previous
             v = inn[j]
             v2 = (v.real*v.real) + (v.imag*v.imag)
-            self.values[self.next] = nd[j]
+            self.values[self.next] = v
             self.value2s[self.next] = v2
 #            self.sum = self.sum + nd[j]
             self.rmssum = self.rmssum + v2
@@ -145,9 +153,9 @@ class ra_event(gr.sync_block):
             if self.next2 >= self.vlen:
                 self.next2 = 0
                     
-            if (self.full):
+            if self.full:
 #                self.ave = self.sum*self.oneovern
-                self.rms2 = self.rmssum*self.oneovern
+                self.rms2 = self.rmssum*self.oneovern2
 #                self.rms = sqrt(self.rms2)
 
                 # now store until all samples processed
@@ -156,11 +164,14 @@ class ra_event(gr.sync_block):
                 outb[j] = self.rms2
                 # finally if full buffer and event found
                 if v2check > self.nsigma2*self.rms2:
+                    self.magnitude = np.sqrt(v2check)
+                    self.stddev = np.sqrt(v2check)
                     self.writeevent()      # record
                     self.initbuffer()      # start again
+                    
         # end for all input samples
         output_items[0] = outa  # put all vectors in output port 0
         output_items[1] = outb  # put all vectors in output port 1
 #        print 'N outputs: ', len(output_items[0]), iout
         return ns
-    # end vsum()
+    # end ra_event work()
